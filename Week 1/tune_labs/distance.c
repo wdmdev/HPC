e8799d910df9d74d8b53b9a2f7e5ae2ce5579d39
William Diedrichsen Marstran <wdmdev@gmail.com>
2023-01-03 15:10:44 +0100

    optimize

--- Week 1/tune_labs/distance.c
+++ Week 1/tune_labs/distance.c
@@ -7,8 +7,12 @@ distance(particle_t *p, int n) {
     
     double dist = 0.0;
 
-    for (int i = 0; i < n; i++)
-        dist += sqrt(p[i].x * p[i].x + p[i].y * p[i].y + p[i].z * p[i].z);
+    for (int i = 0; i < n; i++){
+        double x = p[i].x;
+        double y = p[i].y;
+        double z = p[i].z;
+        dist += sqrt(x * x + y * y + z * z);
+    }
 
     return dist;
 }
@@ -18,8 +22,12 @@ distance(particle_t p, int n) {
 
     double dist = 0.0;
 
-    for (int i = 0; i < n; i++)
-        dist += sqrt(p.x[i]*p.x[i] + p.y[i]*p.y[i] + p.z[i]*p.z[i]);
+    for (int i = 0; i < n; i++){
+        double x = p.x[i];
+        double y = p.y[i];
+        double z = p.z[i];
+        dist += sqrt(x*x + y*y + z*z);
+    }
     
     return dist;
 }
